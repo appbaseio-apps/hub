7766ba280fb97796393afadb4dd7f33471effa1b
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-03-29 18:32:07 +0530

    Fix invalid file import

--- src/@init/App.js
+++ src/@init/App.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import Home from './Home';
 
-import './App.css';
-
 class App extends Component {
 	render() {
 		return (
--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -23,37 +23,37 @@ class Sidebar extends Component {
 				<Menu theme="dark" mode="inline" defaultSelectedKeys={['nodejs']}>
 					{/* <Link to="/"> */}
 					<Menu.Item key="nodejs" className="nodejs">
-						<img src={nodejs} />
+						<img src={nodejs} alt="nodejs" />
 						<span>Node.js</span>
 					</Menu.Item>
 					{/* </Link>
 					<Link to="/go"> */}
 					<Menu.Item key="go" className="go">
-						<img src={go} />
+						<img src={go} alt="go" />
 						<span>Go</span>
 					</Menu.Item>
 					{/* </Link>
 					<Link to="/python"> */}
 					<Menu.Item key="python" className="python">
-						<img src={python} style={{ marginTop: '-0.25rem' }} />
+						<img src={python} style={{ marginTop: '-0.25rem' }} alt="python" />
 						<span>Python</span>
 					</Menu.Item>
 					{/* </Link>
 					<Link to="/php"> */}
 					<Menu.Item key="php" className="php">
-						<img src={php} />
+						<img src={php} alt="php" />
 						<span>PHP</span>
 					</Menu.Item>
 					{/* </Link>
 					<Link to="/ruby"> */}
 					<Menu.Item key="ruby" className="ruby">
-						<img src={ruby} />
+						<img src={ruby} alt="ruby" />
 						<span>Ruby</span>
 					</Menu.Item>
 					{/* </Link>
 					<Link to="/java"> */}
 					<Menu.Item key="java" className="java">
-						<img src={java} />
+						<img src={java} alt="java" />
 						<span>Java</span>
 					</Menu.Item>
 					{/* </Link> */}
